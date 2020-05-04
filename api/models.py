a6f08183ac09dd3e230629be4191b8f03a0f3321
TurboKach <incognito@turbokach.me>
2020-05-04 20:02:02 +0300

    Added Title, Category, Genre, Review models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,5 @@
+import uuid
+
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
@@ -13,10 +15,42 @@ class User(AbstractUser):
     bio = models.TextField(max_length=200, blank=True)
 
 
-class Categories(models.Model):
-    name = models.CharField(max_length=10)
+class Category(models.Model):
+    name = models.CharField(max_length=20)
     slug = models.SlugField()
 
     def __str__(self):
-        return self.slug
+        return self.name
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=20)
+    slug = models.SlugField(unique=True, default=uuid.uuid1)
+
+    def __str__(self):
+        return self.name
+
+
+class Title(models.Model):
+    name = models.TextField(max_length=50)
+    year = models.DateField("Год выпуска")
+    description = models.TextField()
+    genre = models.ForeignKey(
+        Genre, on_delete=models.SET_NULL, related_name="genre_titles", null=True
+    )
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, related_name="category_titles", null=True
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Review(models.Model):
+    title = models.ForeignKey(Title, on_delete=models.CASCADE)
+    text = models.TextField()
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="author_review"
+    )
+    pub_date = models.DateTimeField("Дата добавления", auto_now_add=True, db_index=True)
 
